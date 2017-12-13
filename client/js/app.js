6097a0f39161c8b1f91ca7081a4601dded7e563a
Alex Turevski <me@turevski.com>
2017-12-13 21:44:43 +0200

    Chat - two-legs client

--- client/js/app.js
+++ client/js/app.js
@@ -1,31 +1,121 @@
 // App
 const app = angular.module('app', ['toastr']);
 
-// Service to fetch some data..
-app.factory('messagesService', ['$http', 'toastr', ($http, toastr) => {
+app.factory('messagesService', ['$http', ($http) => {
 	return {
 		get: () => $http.get('/messages'),
-		post: (messageid, text) => {
-			const data = {parentId: messageid, text: text, from: 'TBD'};
-			$http.post('/messages', data),
-			toastr.info(`Sent: ${JSON.stringify(data)}`);
+		post: (data) => {
+			$http.post('/messages', data);
+		}
+	}
+}]);
+
+app.factory('userService', ['$http', 'tools', ($http, tools) => {
+	return {
+		getUserName: () => {
+			let userName = localStorage.getItem("userName");
+			if (userName) {
+				console.log(userName);
+				return Promise.resolve(userName)
+			} else {
+				return $http.get('https://randomuser.me/api/').then((response) => {
+					let data = response.data;
+					let userName = (!data || data.error) ?
+						"Mr. Some One" :
+						tools.toTitleCase(
+							`${data.results[0].name.title}. ${data.results[0].name.first} ${data.results[0].name.last}`);
+					localStorage.setItem("userName", userName);
+					return Promise.resolve(userName);
+				})
+			}
+		},
+		setUserName: (userName) => {
+			localStorage.setItem("userName", userName)
+		}
+	}
+}]);
+
+app.factory('tools', [() => {
+	return {
+		toTitleCase: (str) => {
+			return str.replace(/\w\S*/g, function (txt) {
+				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+			});
 		}
 	}
 }]);
 
 // App controller
-app.controller('appController', ['$scope', 'messagesService', 'toastr', ($scope, data, toastr) => {
-	data.get().success(resp => {
-		$scope.messages = resp.messages;
-		$scope.replying = [];
-		$scope.replyText = "";
+app.controller('appController', [
+	'$scope',
+	'$timeout',
+	'$interval',
+	'messagesService',
+	'toastr',
+	'userService',
+	'tools',
+	($scope, $timeout, $interval, messagesService, toastr, userService, tools) => {
 
 		$scope.replyInputKeyPress = (messageid, $event, replyText) => {
 			if ($event.keyCode == 13) {
 				$scope.replying[messageid] = false;
-				data.post(messageid, replyText);
+				const message = {
+					parentId: messageid,
+					text: replyText,
+					from: $scope.userName
+				};
+				messagesService.post(message);
+				toastr.info(`Sent: ${JSON.stringify(message)}`);
+				//$scope.updateMessages();
 			}
 			return true;
 		};
-	});
-}]);
+
+		$scope.updateMessages = () => {
+			messagesService.get()
+				.then(response => {
+					$timeout(() => {
+						const dataMessages = response.data;
+						$scope.messages = dataMessages.messages;
+					}, 0);
+				});
+		};
+
+		$scope.saveUserName = (userName) => {
+			userService.setUserName(userName);
+		};
+
+		Promise.all([messagesService.get(), userService.getUserName()])
+			.then(responses => {
+
+				$scope.$apply(() => {
+					const dataMessages = responses[0].data;
+					const dataUserName = responses[1];
+
+					const lastIdServer = dataMessages.lastId;
+					$scope.lastIdClient = lastIdServer;
+
+					$scope.messages = dataMessages.messages;
+					$scope.replying = [];
+					$scope.replyText = "";
+					$scope.userName = dataUserName;
+				});
+
+				var theInterval = $interval(() => {
+					messagesService.get()
+						.then(response => {
+							const dataMessages = response.data;
+							const lastIdServer = dataMessages.lastId;
+							if (lastIdServer !== $scope.lastIdClient) {
+								$scope.lastIdClient = lastIdServer;
+								$scope.messages = dataMessages.messages;
+							}
+						});
+				}, 5000);
+
+				$scope.$on('$destroy', function () {
+					$interval.cancel(theInterval)
+				});
+			});
+	}
+]);
--- server/routes.js
+++ server/routes.js
@@ -1,44 +1,63 @@
 module.exports = app => {
 
+	var lastId = 1;
+	var messages = [{
+		id: 1,
+		from: 'Moderator',
+		text: 'Welcome to our chat!',
+		messages: []
+	}];
+
+	var getNewId = function() {
+		return ++lastId;
+	}
+
+	var findMessageById = function(id, arr = messages, level = 1) {
+		if (!arr || !arr.length)
+			return null;
+		let result;
+		for (let i = 0; i < arr.length; i++) {
+			const message = arr[i];
+			if (message.id === id) {
+				result = message;
+				break;
+			} else {
+				if (message.messages) {
+					result = findMessageById(id, message.messages, level + 1);
+					if (result) {
+						break;
+					}
+				}
+			}
+		}
+		return result;
+	}
+
 	app.get(`/`, (req, res) => {
 		res.sendfile('./public/index.html');
 	});
 
-	// const funnyStuff = {
-	// 	question: `Why did the chicken cross the road?`,
-	// 	answer: `To get to the other side`
-	// };
-
-	app.get(`/data`, (req, res) => {
-		res.json(funnyStuff);
-	});
-
 	app.get(`/messages`, (req, res) => {
 		const result = {
-			messages: [{
-					id: 1,
-					from: 'John',
-					text: 'Hello World!',
-					messages: [{
-						id: 11,
-						from: 'Sally',
-						text: 'Hi!'
-					}, {
-						id: 12,
-						from: 'Mary',
-						text: 'Hi there!'
-					}]
-				}, {
-					id: 2,
-					from: 'Term',
-					message: 'I am back!'
-				}
-			]
+			messages: messages,
+			lastId: lastId,
 		};
 		res.json(result);
 	});
 
 	app.post(`/messages`, (req, res) => {
-		console.log(req.jsonBody);
+		let request = req.jsonBody;
+		let parentMessage = findMessageById(request.parentId);
+		if(!parentMessage.messages) {
+			parentMessage.messages = [];
+		}
+		const message = {
+			id: getNewId(),
+			from: request.from,
+			text: request.text,
+			messages: [],
+		};
+		parentMessage.messages.push(message);
+		console.log(message);
 	});
 };
